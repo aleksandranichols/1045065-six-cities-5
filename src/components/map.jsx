f837e38634f508acbd009db5be126723ea199293
aleksandranichols <aleksandra.nichols@gmail.com>
2020-10-22 19:33:47 -0500

    Add proptypes

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,61 +1,65 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { offers } from '../mocks/offers';
+
+import offerProptypes from './offer-proptypes';
 
 class Map extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
+  }
 
-        const {offers} = props;
-    }
+  _displayMapMarkers(map) {
+    const {offers} = this.props;
 
-    _displayMapMarkers(map) {
-        const icon = leaflet.icon({
-            iconUrl: `img/pin.svg`,
-            iconSize: [30, 30]
-        });
+    const icon = leaflet.icon({
+      iconUrl: `img/pin.svg`,
+      iconSize: [30, 30]
+    });
 
-        const offerCords = [52.3709553943508, 4.89309666406198];
-        leaflet
+    const offerCords = [52.3709553943508, 4.89309666406198];
+    leaflet
             .marker(offerCords, {icon})
             .addTo(map);
 
-        offers.map((offer) => {
-            leaflet
+    offers.map((offer) => {
+      leaflet
             .marker(offer.coordinates, {icon})
             .addTo(map);
-        })
-    }
+    });
+  }
 
-    _displayMap() {
-        const city = [52.38333, 4.9];
+  _displayMap() {
+    const city = [52.38333, 4.9];
 
-        const zoom = 12;
-        const map = leaflet.map(`map`, {
-            center: city,
-            zoom: zoom,
-            zoomControl: false,
-            marker: true
-          });
-        map.setView(city, zoom);
+    const zoom = 12;
+    const map = leaflet.map(`map`, {
+      center: city,
+      zoom,
+      zoomControl: false,
+      marker: true
+    });
+    map.setView(city, zoom);
 
-        leaflet
+    leaflet
             .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        })
+              attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+            })
         .addTo(map);
 
-        this._displayMapMarkers(map);
-    }
+    this._displayMapMarkers(map);
+  }
 
-    componentDidMount() {
-        this._displayMap();
-    }
+  componentDidMount() {
+    this._displayMap();
+  }
 
-    render() {
-       return <div id="map"></div>
-    }
+  render() {
+    return <div id="map"></div>;
+  }
 }
+
+Map.propTypes = {offers: PropTypes.arrayOf(offerProptypes)};
 
 export default Map;
